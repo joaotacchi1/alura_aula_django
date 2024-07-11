15d5b1254af72110012da0da9accb5ef6e0fafcd
João Tacchi <tacchijoaovitor@gmail.com>
2024-07-11 16:45:13 -0300

    commit de django com base de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    
+    dados = {
+        1: {
+            'nome': 'Nebulosa de Carina',
+            'legenda': 'webbtelescope.org / NASA / James Webb'
+        },
+        2: {
+            'nome': 'Galáxia do Sombrero',
+            'legenda': 'NASA / Hubble Space Telescope'
+        }
+    }
+    
+    return render(request, 'galeria/index.html', {'cards': dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
